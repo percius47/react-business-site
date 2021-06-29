b058bdb5b3bdc42ee668fb9eba3d17f0ef02247e
percius47 <33738242+percius47@users.noreply.github.com>
2021-06-29 13:36:28 +0530

    update: sections done

---
+++ vouch-site/src/components/InfoSection/Data.js
@@ -0,0 +1,71 @@
+export const homeObjOne={
+    id:'about',
+    lightBg:false,
+    lightText:true,
+    lightTextDesc:true,
+    topLine:'Premium Bank',
+    headLine:'Unlimited transactions with zero fees',
+    description:'Get access to our exclusive app that allows you to rob a bank and not get caught.',
+    buttonLabel:'Get Started',
+    imgStart:false,
+     img: require('../../images/svg-1.svg').default,
+    alt:'Car',
+    dark:true,
+    primary:true,
+    darkText:false
+
+}
+
+export const homeObjTwo={
+    id:'discoveries',
+    lightBg:true,
+    lightText:false,
+    lightTextDesc:false,
+    topLine:'Premium Bank',
+    headLine:'Unlimited transactions with zero fees',
+    description:'Get access to our exclusive app that allows you to rob a bank and not get caught.',
+    buttonLabel:'Get Started',
+    imgStart:true,
+     img: require('../../images/svg-2.svg').default,
+    alt:'img2',
+    dark:false,
+    primary:false,
+    darkText:true
+
+}
+
+export const homeObjThree={
+    id:'about',
+    lightBg:false,
+    lightText:true,
+    lightTextDesc:true,
+    topLine:'Premium Bank',
+    headLine:'Unlimited transactions with zero fees',
+    description:'Get access to our exclusive app that allows you to rob a bank and not get caught.',
+    buttonLabel:'Get Started',
+    imgStart:false,
+     img: require('../../images/svg-3.svg').default,
+    alt:'Car',
+    dark:true,
+    primary:true,
+    darkText:false
+
+}
+
+export const homeObjFour={
+    id:'discoveries',
+    lightBg:true,
+    lightText:false,
+    lightTextDesc:false,
+    topLine:'Premium Bank',
+    headLine:'Unlimited transactions with zero fees',
+    description:'Get access to our exclusive app that allows you to rob a bank and not get caught.',
+    buttonLabel:'Get Started',
+    imgStart:true,
+     img: require('../../images/svg-4.svg').default,
+    alt:'img2',
+    dark:false,
+    primary:false,
+    darkText:true
+
+}
--- vouch-site/src/pages/index.js
+++ vouch-site/src/pages/index.js
@@ -1,9 +1,9 @@
 import React, {useState}from 'react'
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/SideBar';
-import HeroSection
- from '../components/HeroSection';
-
+import HeroSection  from '../components/HeroSection';
+import InfoSection from '../components/InfoSection';
+import { homeObjOne,homeObjTwo,homeObjThree,homeObjFour } from '../components/InfoSection/Data';
 const Home = () => {
 
 const [isOpen, setIsOpen]=useState(false)
@@ -18,6 +18,10 @@ const toggle=()=>{
         <Sidebar isOpen={isOpen} toggle={toggle}/>
         <Navbar toggle={toggle}/>
         <HeroSection/>
+        <InfoSection{...homeObjOne}/>
+        <InfoSection{...homeObjTwo}/>
+        <InfoSection{...homeObjThree}/>
+        <InfoSection{...homeObjFour}/>
       </>
     )
 }
