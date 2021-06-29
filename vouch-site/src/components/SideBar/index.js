cf4a032fe076721107fbc244c07f6b5dcf7f0ce3
percius47 <33738242+percius47@users.noreply.github.com>
2021-06-29 05:44:10 +0530

    update: navbar & sidebar done

---
+++ vouch-site/src/components/SideBar/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { SidebarContainer, CloseIcon, Icon,
+SideBtnWrap,
+SidebarLink,
+SidebarWrapper,
+SidebarRoute,
+SidebarMenu
+} from './SidebarElements'
+
+
+const Sidebar = ({isOpen, toggle}) => {
+    return (
+        <SidebarContainer isOpen={isOpen} onClick={toggle}>
+            <Icon onClick={toggle}>
+                <CloseIcon/>
+            </Icon>
+            <SidebarWrapper>
+                <SidebarMenu>
+                    <SidebarLink to="about" onClick={toggle}>
+                    About
+                    </SidebarLink>
+                    <SidebarLink to="discoveries"onClick={toggle}>
+                    Discoveries
+                    </SidebarLink> <SidebarLink to="services"onClick={toggle}>
+                    Services
+                    </SidebarLink> <SidebarLink to="signup"onClick={toggle}>
+                    Sign Up
+                    </SidebarLink>
+                </SidebarMenu>
+                <SideBtnWrap>
+                    <SidebarRoute to="/signin">Sign In</SidebarRoute>
+                </SideBtnWrap>
+            </SidebarWrapper>
+        </SidebarContainer>
+    )
+}
+
+export default Sidebar
